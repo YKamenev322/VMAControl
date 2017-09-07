8e377106eade40a266d9ac85e7fe95371e1de0c6
hells1ng <hells1ng43@mail.ru>
2017-09-07 18:53:28 +0300

    add stm32Flasher

---
+++ stm32Flasher/moc_mainwindow.cpp
@@ -0,0 +1,263 @@
+/****************************************************************************
+** Meta object code from reading C++ file 'mainwindow.h'
+**
+** Created by: The Qt Meta Object Compiler version 67 (Qt 5.3.2)
+**
+** WARNING! All changes made in this file will be lost!
+*****************************************************************************/
+
+#include "../../stm32Flasher/mainwindow.h"
+#include <QtCore/qbytearray.h>
+#include <QtCore/qmetatype.h>
+#if !defined(Q_MOC_OUTPUT_REVISION)
+#error "The header file 'mainwindow.h' doesn't include <QObject>."
+#elif Q_MOC_OUTPUT_REVISION != 67
+#error "This file was generated using the moc from 5.3.2. It"
+#error "cannot be used with the include files from this version of Qt."
+#error "(The moc has changed too much.)"
+#endif
+
+QT_BEGIN_MOC_NAMESPACE
+struct qt_meta_stringdata_MainWindow_t {
+    QByteArrayData data[42];
+    char stringdata[541];
+};
+#define QT_MOC_LITERAL(idx, ofs, len) \
+    Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
+    qptrdiff(offsetof(qt_meta_stringdata_MainWindow_t, stringdata) + ofs \
+        - idx * sizeof(QByteArrayData)) \
+    )
+static const qt_meta_stringdata_MainWindow_t qt_meta_stringdata_MainWindow = {
+    {
+QT_MOC_LITERAL(0, 0, 10),
+QT_MOC_LITERAL(1, 11, 7),
+QT_MOC_LITERAL(2, 19, 0),
+QT_MOC_LITERAL(3, 20, 4),
+QT_MOC_LITERAL(4, 25, 4),
+QT_MOC_LITERAL(5, 30, 6),
+QT_MOC_LITERAL(6, 37, 14),
+QT_MOC_LITERAL(7, 52, 5),
+QT_MOC_LITERAL(8, 58, 13),
+QT_MOC_LITERAL(9, 72, 17),
+QT_MOC_LITERAL(10, 90, 17),
+QT_MOC_LITERAL(11, 108, 17),
+QT_MOC_LITERAL(12, 126, 20),
+QT_MOC_LITERAL(13, 147, 19),
+QT_MOC_LITERAL(14, 167, 16),
+QT_MOC_LITERAL(15, 184, 16),
+QT_MOC_LITERAL(16, 201, 17),
+QT_MOC_LITERAL(17, 219, 20),
+QT_MOC_LITERAL(18, 240, 19),
+QT_MOC_LITERAL(19, 260, 23),
+QT_MOC_LITERAL(20, 284, 11),
+QT_MOC_LITERAL(21, 296, 15),
+QT_MOC_LITERAL(22, 312, 10),
+QT_MOC_LITERAL(23, 323, 12),
+QT_MOC_LITERAL(24, 336, 3),
+QT_MOC_LITERAL(25, 340, 3),
+QT_MOC_LITERAL(26, 344, 1),
+QT_MOC_LITERAL(27, 346, 9),
+QT_MOC_LITERAL(28, 356, 8),
+QT_MOC_LITERAL(29, 365, 12),
+QT_MOC_LITERAL(30, 378, 14),
+QT_MOC_LITERAL(31, 393, 14),
+QT_MOC_LITERAL(32, 408, 18),
+QT_MOC_LITERAL(33, 427, 24),
+QT_MOC_LITERAL(34, 452, 6),
+QT_MOC_LITERAL(35, 459, 13),
+QT_MOC_LITERAL(36, 473, 10),
+QT_MOC_LITERAL(37, 484, 10),
+QT_MOC_LITERAL(38, 495, 11),
+QT_MOC_LITERAL(39, 507, 10),
+QT_MOC_LITERAL(40, 518, 10),
+QT_MOC_LITERAL(41, 529, 11)
+    },
+    "MainWindow\0newFile\0\0open\0save\0saveAs\0"
+    "openRecentFile\0about\0refreshSerial\0"
+    "transaction_reset\0transaction_erase\0"
+    "transaction_flash\0transaction_flashall\0"
+    "transaction_gotoapp\0transaction_send\0"
+    "transaction_read\0transaction_write\0"
+    "transaction_writeall\0transaction_sendVel\0"
+    "transaction_sendVel_All\0start_timer\0"
+    "start_timer_All\0stop_timer\0showResponse\0"
+    "CMD\0req\0s\0respBytes\0showFail\0processError\0"
+    "processTimeout\0on_cleanButton\0"
+    "on_reloadHexButton\0on_enableNetworkCheckBox\0"
+    "enable\0enableButtons\0setVel1Max\0"
+    "setVel1Min\0setVel1Null\0setVel2Max\0"
+    "setVel2Min\0setVel2Null"
+};
+#undef QT_MOC_LITERAL
+
+static const uint qt_meta_data_MainWindow[] = {
+
+ // content:
+       7,       // revision
+       0,       // classname
+       0,    0, // classinfo
+      35,   14, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       0,       // signalCount
+
+ // slots: name, argc, parameters, tag, flags
+       1,    0,  189,    2, 0x08 /* Private */,
+       3,    0,  190,    2, 0x08 /* Private */,
+       4,    0,  191,    2, 0x08 /* Private */,
+       5,    0,  192,    2, 0x08 /* Private */,
+       6,    0,  193,    2, 0x08 /* Private */,
+       7,    0,  194,    2, 0x08 /* Private */,
+       8,    0,  195,    2, 0x08 /* Private */,
+       9,    0,  196,    2, 0x08 /* Private */,
+      10,    0,  197,    2, 0x08 /* Private */,
+      11,    0,  198,    2, 0x08 /* Private */,
+      12,    0,  199,    2, 0x08 /* Private */,
+      13,    0,  200,    2, 0x08 /* Private */,
+      14,    0,  201,    2, 0x08 /* Private */,
+      15,    0,  202,    2, 0x08 /* Private */,
+      16,    0,  203,    2, 0x08 /* Private */,
+      17,    0,  204,    2, 0x08 /* Private */,
+      18,    0,  205,    2, 0x08 /* Private */,
+      19,    0,  206,    2, 0x08 /* Private */,
+      20,    0,  207,    2, 0x08 /* Private */,
+      21,    0,  208,    2, 0x08 /* Private */,
+      22,    0,  209,    2, 0x08 /* Private */,
+      23,    4,  210,    2, 0x08 /* Private */,
+      28,    1,  219,    2, 0x08 /* Private */,
+      29,    1,  222,    2, 0x08 /* Private */,
+      30,    1,  225,    2, 0x08 /* Private */,
+      31,    0,  228,    2, 0x08 /* Private */,
+      32,    0,  229,    2, 0x08 /* Private */,
+      33,    1,  230,    2, 0x08 /* Private */,
+      35,    0,  233,    2, 0x08 /* Private */,
+      36,    0,  234,    2, 0x08 /* Private */,
+      37,    0,  235,    2, 0x08 /* Private */,
+      38,    0,  236,    2, 0x08 /* Private */,
+      39,    0,  237,    2, 0x08 /* Private */,
+      40,    0,  238,    2, 0x08 /* Private */,
+      41,    0,  239,    2, 0x08 /* Private */,
+
+ // slots: parameters
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void, QMetaType::Int, QMetaType::QString, QMetaType::QString, QMetaType::QByteArray,   24,   25,   26,   27,
+    QMetaType::Void, QMetaType::QString,   26,
+    QMetaType::Void, QMetaType::QString,   26,
+    QMetaType::Void, QMetaType::QString,   26,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void, QMetaType::Bool,   34,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+
+       0        // eod
+};
+
+void MainWindow::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        MainWindow *_t = static_cast<MainWindow *>(_o);
+        switch (_id) {
+        case 0: _t->newFile(); break;
+        case 1: _t->open(); break;
+        case 2: _t->save(); break;
+        case 3: _t->saveAs(); break;
+        case 4: _t->openRecentFile(); break;
+        case 5: _t->about(); break;
+        case 6: _t->refreshSerial(); break;
+        case 7: _t->transaction_reset(); break;
+        case 8: _t->transaction_erase(); break;
+        case 9: _t->transaction_flash(); break;
+        case 10: _t->transaction_flashall(); break;
+        case 11: _t->transaction_gotoapp(); break;
+        case 12: _t->transaction_send(); break;
+        case 13: _t->transaction_read(); break;
+        case 14: _t->transaction_write(); break;
+        case 15: _t->transaction_writeall(); break;
+        case 16: _t->transaction_sendVel(); break;
+        case 17: _t->transaction_sendVel_All(); break;
+        case 18: _t->start_timer(); break;
+        case 19: _t->start_timer_All(); break;
+        case 20: _t->stop_timer(); break;
+        case 21: _t->showResponse((*reinterpret_cast< int(*)>(_a[1])),(*reinterpret_cast< const QString(*)>(_a[2])),(*reinterpret_cast< const QString(*)>(_a[3])),(*reinterpret_cast< QByteArray(*)>(_a[4]))); break;
+        case 22: _t->showFail((*reinterpret_cast< const QString(*)>(_a[1]))); break;
+        case 23: _t->processError((*reinterpret_cast< const QString(*)>(_a[1]))); break;
+        case 24: _t->processTimeout((*reinterpret_cast< const QString(*)>(_a[1]))); break;
+        case 25: _t->on_cleanButton(); break;
+        case 26: _t->on_reloadHexButton(); break;
+        case 27: _t->on_enableNetworkCheckBox((*reinterpret_cast< bool(*)>(_a[1]))); break;
+        case 28: _t->enableButtons(); break;
+        case 29: _t->setVel1Max(); break;
+        case 30: _t->setVel1Min(); break;
+        case 31: _t->setVel1Null(); break;
+        case 32: _t->setVel2Max(); break;
+        case 33: _t->setVel2Min(); break;
+        case 34: _t->setVel2Null(); break;
+        default: ;
+        }
+    }
+}
+
+const QMetaObject MainWindow::staticMetaObject = {
+    { &QMainWindow::staticMetaObject, qt_meta_stringdata_MainWindow.data,
+      qt_meta_data_MainWindow,  qt_static_metacall, 0, 0}
+};
+
+
+const QMetaObject *MainWindow::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+}
+
+void *MainWindow::qt_metacast(const char *_clname)
+{
+    if (!_clname) return 0;
+    if (!strcmp(_clname, qt_meta_stringdata_MainWindow.stringdata))
+        return static_cast<void*>(const_cast< MainWindow*>(this));
+    return QMainWindow::qt_metacast(_clname);
+}
+
+int MainWindow::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QMainWindow::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 35)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 35;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 35)
+            *reinterpret_cast<int*>(_a[0]) = -1;
+        _id -= 35;
+    }
+    return _id;
+}
+QT_END_MOC_NAMESPACE
